ccd0d3dfcc73b6b5d1c4cf9f615aaae1ffc1492f
justbelieveme <justbelieveme@mitac.com.tw>
2020-08-07 15:11:47 +0800

    queensland added

--- server.js
+++ server.js
@@ -43,8 +43,46 @@ app.get('/tainan/detail_en',function(req,res){
 
 //Australia english part
 app.get('/au',function(req,res){
-    res.sendFile(__dirname+ "/index_australia.html")
+    res.sendFile(__dirname + "/index_australia.html")
 })
+
+app.get('/au/queensland', function(req, res){
+    res.sendFile(__dirname + "/index_queensland.html")
+})
+
+app.get('/au/queensland/brisbane', function(req, res){
+    res.sendFile(__dirname + "/index_brisbane.html")
+})
+
+app.get('/au/queensland/brisbane/redland', function(req, res){
+    res.sendFile(__dirname + "/index_redland.html")
+})
+
+app.get('/au/queensland/brisbane/redland/detailed_redland_en', function(req, res){
+    res.sendFile(__dirname + "/detailed_redland_en.html")
+})
+
+// Australia zh part
+app.get('/au_zh', function(req, res){
+    res.sendFile(__dirname + "/index_australia_zh.html")
+})
+
+app.get('/au_zh/queensland_zh', function(req, res){
+    res.sendFile(__dirname + "/index_queensland_zh.html")
+})
+
+app.get('/au_zh/queensland_zh/brisbane_zh', function(req, res){
+    res.sendFile(__dirname + "/index_brisbane_zh.html")
+})
+
+app.get('/au_zh/queensland_zh/brisbane_zh/redland_zh', function(req, res){
+    res.sendFile(__dirname + "/index_redland_zh.html")
+})
+
+app.get('/au_zh/queensland_zh/brisbane_zh/redland_zh/detailed_redland_zh', function(req, res){
+    res.sendFile(__dirname + "/detailed_redland_zh.html")
+})
+
 
 
 app.listen(3000 ,function(){
